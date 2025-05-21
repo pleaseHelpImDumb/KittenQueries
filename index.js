4e30022457892047f2308e23028ed9e8119d8670
pleaseHelpImDumb <31854027+pleaseHelpImDumb@users.noreply.github.com>
2025-05-21 15:29:40 -0400

    Breed selector, breed fact, and styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {API_KEY} from './config.js'
+import {API_BREEDS_KEY, API_KEY} from './config.js'
 console.log(API_KEY); // Should now show your API key
 
 //Random Cat Image
@@ -11,9 +11,51 @@ randomCatImage.width = 400;
 const refreshButton = document.createElement("button");
 refreshButton.innerHTML = "Refresh";
 body.appendChild(refreshButton);
-refreshButton.addEventListener("click", refreshImage);
+refreshButton.addEventListener("click", getBreedImage);
 
+//Breed selector
+const breedDropdownLabel = document.createElement("label");
+const breedDropdown = document.createElement("select");
+breedDropdown.id = "breedSelector";
+breedDropdownLabel.htmlFor = "breedSelector";
+breedDropdownLabel.innerHTML = "Choose a breed!";
+const randomOption = document.createElement("option");
+randomOption.innerHTML = "Random";
+randomOption.value = "rand";
+breedDropdown.appendChild(randomOption);
+
+body.appendChild(breedDropdownLabel);
+body.appendChild(breedDropdown);
 body.appendChild(randomCatImage);
+
+const description = document.createElement("p");
+description.id = "breedInfo";
+
+body.appendChild(description);
+
+
+fetch(API_BREEDS_KEY)
+    .then(res => {
+        if(!res.ok){
+            throw new Error ("Bad request");
+        }
+        else return res.json();
+    })
+    .then(data =>{
+        data.forEach(e =>{
+            const breedOption = document.createElement("option");
+            breedOption.innerHTML = e.name;
+            breedOption.value = e.id;
+            breedDropdown.appendChild(breedOption);
+        }
+        )
+    })
+    .catch(error => {
+        console.error(error);
+    }
+    )
+
+
 
 fetch(API_KEY)
     .then(res => {
@@ -23,7 +65,7 @@ fetch(API_KEY)
         else return res.json();
     })
     .then(data => {
-        console.log(data[0]);
+        console.log(data);
         randomCatImage.src = data[0].url;
     })
     .catch(error => {
@@ -31,8 +73,38 @@ fetch(API_KEY)
     });
 
 
-async function refreshImage(){
-    const imgData = await fetch(API_KEY);
+// async function refreshImage(){
+//     const imgData = await fetch(API_KEY);
+//     const data = await imgData.json();
+//     randomCatImage.src = data[0].url;
+// }
+
+async function getBreedImage(){
+    console.log("getting breed for: " + breedDropdown.value);
+    let imgData;
+    if(breedDropdown.value == "rand"){
+        imgData = await fetch(API_KEY);
+    }
+    else{
+        description.innerHTML = await getBreedDescription(breedDropdown.value); 
+        imgData = await fetch(API_KEY+"?breed_ids="+breedDropdown.value);
+    }
     const data = await imgData.json();
     randomCatImage.src = data[0].url;
+    body.appendChild(description);
+}
+
+async function getBreedDescription(breedId){
+    const breedData = await fetch(API_BREEDS_KEY);
+    const data = await breedData.json();
+    console.log(data);
+    const foundBreed = data.find(e => e.id == breedId);
+
+    if(foundBreed){
+        return foundBreed.description;
+    }
+    else{
+        return "";
+    }
+
 }
